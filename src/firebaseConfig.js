781529839cd2d9137ca7ee2583676e3839986ac9
Nilesh Gurav <nileshgurav1718@gmail.com>
2024-11-04 20:05:01 +0530

    firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcA2zFd5HD4HmP9vU9EdUs8OLSFTYU7BA",
-  authDomain: "signindemo-38d39.firebaseapp.com",
-  projectId: "signindemo-38d39",
-  storageBucket: "signindemo-38d39.appspot.com", 
-  messagingSenderId: "266636154517",
-  appId: "1:266636154517:web:80adaebd5c900a1a012d9c",
-  measurementId: "G-NQ89V4B5R9"
+//Your Config
 };
 
 const app = initializeApp(firebaseConfig);
